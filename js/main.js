f11d6a224671c7f334dc9be21609219c4ce0d9ef
Sunna Rún Pétursdóttir <sunnarunpeturs@gmail.com>
2018-12-03 21:42:16 +0000

    Now only showing shows that have "now_showing"
    now_showing field added to the json of shows,
    now only showing those that have now_showing true
    on the page.

--- js/main.js
+++ js/main.js
@@ -10,30 +10,47 @@ var cards =[
 {
 	names:'Svartlyng',
 	img:'img/cards/1.png',
+	now_showing:true
 },{
 	names:'Sol',
 	img:'img/cards/2.png',
+	now_showing:true
 },{
 	names:'Tómir vasar',
 	img:'img/cards/3.png',
+	now_showing:true
 },{
 	names:'Crescendo',
 	img:'img/cards/4.png',
+	now_showing:true
 },{
 	names:'Rejúníon',
 	img:'img/cards/5.png',
+	now_showing:true
 },{
 	names:'The Vallay',
 	img:'img/cards/6.png',
+	now_showing:true
 },{
 	names:'Hafið bláa',
 	img:'img/cards/7.png',
+	now_showing:true
 },{
 	names:'Persónur',
 	img:'img/cards/8.png',
+	now_showing:true
 },{
 	names:'Svartlyng',
 	img:'img/cards/9.png',
+	now_showing:true
+},{
+	names:'Persónur2',
+	img:'img/cards/8.png',
+	now_showing:false
+},{
+	names:'Svartlyng2',
+	img:'img/cards/9.png',
+	now_showing:false
 }
 
 ]
@@ -46,6 +63,7 @@ window.addEventListener("load", function () {
 // loopar í gegnum array og rúllar í hverju load
 var show = function(){
 for(var i = 0; i < cards.length; i++){
+	if(cards[i].now_showing){
 	cardContainer.innerHTML +=`
 			<div class="card i-syningu">		
 					<img src="${cards[i].img}">
@@ -53,6 +71,7 @@ for(var i = 0; i < cards.length; i++){
 					<h4>${cards[i].names}</h4>			
 			</div>
 	`
+	}
 }
 }
 //hringekja
